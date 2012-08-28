e80d6987e6371443b562f9473d4b86297c31a8f4
Den <xian.den@gmail.com>
2012-08-28 03:42:19 +0300

    update header design

--- module/friend/template/pautina/block/notify.html.php
+++ module/friend/template/pautina/block/notify.html.php
@@ -13,7 +13,10 @@ defined('PHPFOX') or exit('NO DICE!');
 ?>
 <li>	
 	<span class="holder_notify_count" id="js_total_new_friend_requests">0</span>
-	<a href="#" title="{phrase var='friend.friend_requests'}" class="friend_notification notify_drop_link" rel="friend.getRequests">{phrase var='friend.friend_requests'}<span>Люди</span></span></a>
+	<a href="#" title="{phrase var='friend.friend_requests'}" class="friend_notification notify_drop_link" rel="friend.getRequests">
+        <span class="notify_image">&nbsp;</span>
+        <span class="notify_title">Люди</span>
+    </a>
 	<div class="holder_notify_drop">
 		<div class="holder_notify_drop_content">
 			<div class="holder_notify_drop_title">{phrase var='friend.friend_requests'}</div>
