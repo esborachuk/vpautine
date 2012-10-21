0b1022995ec2745a9f318e04e916c444c9d395cf
debn <xian.den@gmail.com>
2012-10-21 19:40:20 +0300

    update image box - add count of likes and comments;

--- module/pautina/template/default/block/profile/imagebox.html.php
+++ module/pautina/template/default/block/profile/imagebox.html.php
@@ -10,6 +10,14 @@
                 <a href="<?php echo $photo['link']; ?>" rel="<?php echo $photo['photo_id']; ?>" class="thickbox photo_holder_image" title="<?php echo $photo['title'] ?>">
                     <img src="<?php echo $photo['image_src_small'] ?>" alt="<?php echo $photo['title'] ?>">
                 </a>
+
+                <?php if ($photo['total_like'] > 0): ?>
+                    <span class="like"><?php echo $photo['total_like']; ?></span>
+                <?php endif; ?>
+
+                <?php if ($photo['total_comment'] > 0): ?>
+                    <span class="comments"><?php echo $photo['total_comment']; ?></span>
+                <?php endif; ?>
             </li>
         <?php endforeach; ?>
     </ul>
