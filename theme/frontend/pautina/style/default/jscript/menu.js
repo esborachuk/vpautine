672a9eb73f2b83312fccd5e02ee10a8241e6cd7f
Andrew Sirik <asirik86@gmail.com>
2012-10-28 00:57:22 +0300

    added display non for menu

--- theme/frontend/pautina/style/default/jscript/menu.js
+++ theme/frontend/pautina/style/default/jscript/menu.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function(){
-    jQuery("a#call-menu").click(function(){
-        jQuery("div#user-menu").toggle("fast");
+jQuery(document).ready(function () {
+    jQuery("a#call-menu").click(function () {
+        jQuery("div#user-menu").toggle("slow");
         jQuery(this).toggleClass("active");
         return false;
     });
