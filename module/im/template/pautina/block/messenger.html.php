13360157c5efecac9d7b16ad7285ccccb1bd0874
Andrew Sirik <asirik86@gmail.com>
2012-11-10 16:20:37 +0200

    added html markup for chat border

--- module/im/template/pautina/block/messenger.html.php
+++ module/im/template/pautina/block/messenger.html.php
@@ -11,12 +11,14 @@
 defined('PHPFOX') or exit('NO DICE!'); 
 
 ?>
-<div id="js_footer_im_holder" class="im_holder js_footer_holder">
-	<div  class="im_header" id="js_main_chat_header">
-        <div class="close-chat"><a onclick="$Core.im.toggleMessengerLink(); return false;" href="#"><img src="/theme/frontend/pautina/style/default/image/layout/im_close.png" alt="" class="v_middle close_panel"></a></div>
-		{phrase var='im.chat'}		
-	</div>
-	<div style="overflow:auto; height:355px;" id="js_im_friend_list">
-		{module name='im.list'}
-	</div>
+<div class="main_chat">
+    <div id="js_footer_im_holder" class="im_holder js_footer_holder">
+        <div  class="im_header" id="js_main_chat_header">
+            <div class="close-chat"><a onclick="$Core.im.toggleMessengerLink(); return false;" href="#"><img src="/theme/frontend/pautina/style/default/image/layout/im_close.png" alt="" class="v_middle close_panel"></a></div>
+            {phrase var='im.chat'}
+        </div>
+        <div style="overflow:auto; height:355px;" id="js_im_friend_list">
+            {module name='im.list'}
+        </div>
+    </div>
 </div>
