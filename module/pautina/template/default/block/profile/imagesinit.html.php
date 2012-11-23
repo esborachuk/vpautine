afbfafe99a5b0a2f2ec691c5c98f12c4abfb9b4b
Den Kezlya <xian.den@gmail.com>
2012-11-23 15:36:43 +0200

    pautina-9 Show new photos on scroll page

--- module/pautina/template/default/block/profile/imagesinit.html.php
+++ module/pautina/template/default/block/profile/imagesinit.html.php
@@ -3,11 +3,11 @@
 </script>
 
 
-<div id="scrollbar1">
+<div id="scrollbar_wrapper" class="photo">
     <div class="scrollbar"><div class="track"><div class="thumb"><div class="end"></div></div></div></div>
     <div class="viewport">
         <div class="overview">
-            <div id="bom"></div>
+            <div id="ajax_wrapper"></div>
         </div>
     </div>
 </div>
