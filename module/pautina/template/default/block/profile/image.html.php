df18be0b2e1a6e6ae67b47735dfd73b81b2a98be
Andrew Sirik <asirik86@gmail.com>
2012-11-25 16:03:00 +0200

    added block for button profile

--- module/pautina/template/default/block/profile/image.html.php
+++ module/pautina/template/default/block/profile/image.html.php
@@ -21,11 +21,49 @@ defined('PHPFOX') or exit('NO DICE!');
 		{else}
 			{$sProfileImage}
 		{/if}
+        <div class="p_4">
+            <a href="{url link='user.photo'}">{phrase var='profile.change_picture'}</a>
+        </div>
     </div>
-	{if Phpfox::getUserId() == $aUser.user_id}
-	<div class="p_4">
+
+
+
+	{if !(Phpfox::getUserId() == $aUser.user_id)}
+	<!--<div class="p_4">
 		<a href="{url link='user.photo'}">{phrase var='profile.change_picture'}</a>
-	</div>
+	</div>-->
+    <div class="buttons_user">
+        {if Phpfox::isModule('mail') && Phpfox::getService('user.privacy')->hasAccess('' . $aUser.user_id . '', 'mail.send_message')}
+       <ul>
+        <li id="liMail" class="profile-section-menu"><a href="#" onclick="$Core.composeMessage({left_curly}user_id: {$aUser.user_id}{right_curly}); return false;">
+            <span>{phrase var='profile.send_message'}</span>
+        </a>
+        </li>
+        {/if}
+        {if Phpfox::isModule('friend') && (!$aUser.is_friend || $aUser.is_friend === 3)}
+        <li id="js_add_friend_on_profile" class='profile-section-menu {if $aUser.is_friend === 3}  "js_profile_online_friend_request"{/if}'>
+            <a href="#" onclick="return $Core.addAsFriend('{$aUser.user_id}');" title="{phrase var='profile.add_to_friends'}">
+                <span>{if $aUser.is_friend === 3}{phrase var='profile.confirm_friend_request'}{else}{phrase var='profile.add_to_friends'}{/if}</span>
+            </a>
+        </li>
+        {/if}
+        {if $bCanPoke && Phpfox::getService('user.privacy')->hasAccess('' . $aUser.user_id . '', 'poke.can_send_poke')}
+        <li id="liPoke" class="profile-section-menu">
+            <a href="#" id="section_poke" onclick="$.ajaxCall('poke.doPoke', 'user_id=10', 'GET');tb_remove(); return false;">
+                <span>{phrase var='poke.poke' full_name=''}</span>
+            </a>
+        </li>
+        {/if}
+        {plugin call='profile.template_block_menu_more'}
+        {if (Phpfox::getUserParam('user.can_block_other_members') && Phpfox::getUserGroupParam('' . $aUser.user_group_id . '', 'user.can_be_blocked_by_others'))
+        || (isset($aUser.is_online) && $aUser.is_online && Phpfox::isModule('im') && Phpfox::getParam('im.enable_im_in_footer_bar') && $aUser.is_friend == 1)
+        || (Phpfox::getUserParam('user.can_feature'))
+        || (isset($bPassMenuMore))
+        }
+        <li><a href="#" id="section_menu_more" class="js_hover_title"><span class="section_menu_more_image"></span><span class="js_hover_info">{phrase var='profile.more'}</span>Больше</a></li>
+        {/if}
+       </ul>
+    </div>
 	{/if}
 </div>
 {/if}
