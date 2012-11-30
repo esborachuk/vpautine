72553fb91e191b7ac1bde6c67d91077c77810bb3
Andrew Sirik <asirik86@gmail.com>
2012-12-01 01:25:41 +0200

    added script for button - change cover

--- theme/frontend/pautina/style/default/jscript/menu.js
+++ theme/frontend/pautina/style/default/jscript/menu.js
@@ -16,3 +16,27 @@ MenuOut = {
         $(MenuOut.Hover).animate({'right':'-59px'},300);
     }
 };
+
+$(document).ready(function() {
+    ChangeCover.init();
+});
+
+ChangeCover = {
+    coverLink: '#section_menu',
+    coverSection: '#profile_header_logo',
+    init: function(){
+      if( $(ChangeCover.coverSection).height() > 0){
+          // $(ChangeCover.coverLink).hide();
+           var y =  $(ChangeCover.coverSection).height()/1.4;    //detect height of block with photo
+           var x =  $(ChangeCover.coverSection).width();         //detect width
+           $(ChangeCover.coverLink).css({'top': y});
+
+        $(ChangeCover.coverSection).mouseenter(function(){
+            $(ChangeCover.coverLink).show();
+        }).mouseleave(function() {
+                $(ChangeCover.coverLink).hide();
+            }) }else{
+          $(ChangeCover.coverLink).show();
+      }
+    }
+};
