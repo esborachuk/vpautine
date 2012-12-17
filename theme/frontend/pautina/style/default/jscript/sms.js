3d800c85ea322cced35cb8cf0e048a7196b80a4d
home-work <azsxdcfv123>
2012-12-18 00:54:32 +0200

    update sms module

--- theme/frontend/pautina/style/default/jscript/sms.js
+++ theme/frontend/pautina/style/default/jscript/sms.js
@@ -7,31 +7,35 @@ Sms = {
     smsController: 'sms.smsProcess',
 
     init: function() {
-        $("#tabs").tabs();
+        $("#tabs").tabs({
+            load: function() {
+                $('#messages_block').show();
+            }
+        });
 
         $(Sms.smsForm).validate({
             rules: {
                 'sms[phone]': {
                     required: true,
                     digits: true,
-                    minlength: 10,
-                    maxlength: 10
+                    minlength: 7,
+                    maxlength: 7
                 },
                 'sms[message]': {
                     required: true,
-                    maxlength: 140
+                    maxlength: 160
                 }
             },
             messages: {
                 'sms[phone]': {
                     required: "это обязательное поле",
                     digits: "доступны только цифры",
-                    minlength: "минимаотная длинна номера 10 цифр",
-                    maxlength: "максимальная длина номера 10 цифр"
+                    minlength: "минимаотная длинна номера 7 цифр",
+                    maxlength: "максимальная длина номера 7 цифр"
                 },
                 'sms[message]': {
                     required: "это обязательное поле",
-                    maxlength: "максимальная сообщения номера 140 символов"
+                    maxlength: "максимальная сообщения номера 160 символов"
                 }
             }
         });
