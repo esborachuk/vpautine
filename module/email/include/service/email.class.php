33cc8d65dbe6b9dc79b503d6aabf1748609dcde3
home-work <azsxdcfv123>
2012-12-18 00:47:30 +0200

    update email module

--- module/email/include/service/email.class.php
+++ module/email/include/service/email.class.php
@@ -27,12 +27,12 @@ class Email_Service_Email extends Phpfox_Service
         $sendToUser = $userService->getUser($this->emailInfo['user-id']);
         $sendToEmail = $sendToUser['email'];
         $sendFromUser = $userService->getUser(Phpfox::getUserId());
-        $sendFromUserLink = '<a href="' . $userService->getLink($sendFromUser['user_id']) . '">' . $sendFromUser['full_name'] . '</a>';
+        $sendFromUserLink = 'Отправлено пользователем <a href="' . $userService->getLink($sendFromUser['user_id']) . '">' . $sendFromUser['full_name'] . '</a>';
 
         $email = Phpfox::getLib('mail')
                     ->to($sendToEmail)
-                    ->subject($this->emailInfo['title'] . '. ' . $sendFromUser['full_name'])
-                    ->message($this->emailInfo['message'] . '<br />' . $sendFromUserLink)
+                    ->subject($this->emailInfo['title'])
+                    ->message($this->emailInfo['message'] . '<br /><br />' . $sendFromUserLink)
                     ->send();
 
         return $email;
