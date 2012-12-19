5a644c091f34a5becc982722dbd12924012dacf8
bboyforrest <102030forr>
2012-12-20 01:03:10 +0200

    Added new language xml files (Email, SMS modules)

--- module/email/template/default/block/email.html.php
+++ module/email/template/default/block/email.html.php
@@ -7,27 +7,23 @@
     <?php endif; ?>
     <div class="table">
         <div id="email-error"></div>
-        <div class="table_left">
-            Тема:
-        </div>
+        <div class="table_left">{phrase var='email.message_subject'}</div>
         <div class="table_right">
             <input type="text" name="email[title]" id="email-title" />
         </div>
         <div class="clear"></div>
 
-        <label for="email[message]" class="table_left">
-            Письмо:
-        </label>
+        <label for="email[message]" class="table_left">{phrase var='email.letter'}</label>
         <div class="table_right">
             <textarea name="email[message]" id="email-message" rows="8" cols="50"></textarea>
         </div>
         <div id="email-button">
-            <input type="submit" class="button" value="Отправить Email" onclick="Email.send(); return false;" />
+            <input type="submit" class="button" value="{phrase var='email.send__email'}" onclick="Email.send(); return false;" />
         </div>
         <div class="clear"></div>
     </div>
 </form>
 <div id="email-success-result">
     <div class="message"></div>
-    <a id="sendMoreEmail" href="#" onclick="Email.sendMoreEmail(); return false;">Отправить еще Email</a>
+    <a id="sendMoreEmail" href="#" onclick="Email.sendMoreEmail(); return false;">{phrase var='email.send_new_email'}</a>
 </div>
--- module/sms/template/default/block/sms.html.php
+++ module/sms/template/default/block/sms.html.php
@@ -7,32 +7,28 @@
     <?php endif; ?>
     <div class="table">
         <div id="sms-error"></div>
-        <div class="table_left">
-            Номер телефона:
-        </div>
+        <div class="table_left">{phrase var='sms.phone_number'}</div>
         <div class="sms_number_block table_right">
             <select name="sms[county]">
-                <option value="38">Украина</option>
-                <option value="7">Россия</option>
+                <option value="38">{phrase var='sms.country_code_ua'}</option>
+                <option value="7">{phrase var='sms.country_code_ru'}</option>
             </select>
-            <input placeholder="0991234567" type="text" name="sms[phone]" id="sms-phone" {if isset($phoneNumber)}value="{$phoneNumber}"{/if} />
+            <input placeholder="{phrase var='sms.phone_number_example'}" type="text" name="sms[phone]" id="sms-phone" {if isset($phoneNumber)}value="{$phoneNumber}"{/if} />
         </div>
         <div class="clear"></div>
         <div class="sms_block_message">
-            <label for="sms[message]" class="table_left">
-                Сообщение:
-            </label>
+            <label for="sms[message]" class="table_left">{phrase var='sms.message_text'}</label>
             <div class="table_right">
                 <textarea name="sms[message]" id="sms-message" rows="5" cols="50"></textarea>
             </div>
         </div>
         <div id="sms-button">
-            <input type="submit" class="button" value="Отправить Sms" onclick="Sms.send(); return false;" />
+            <input type="submit" class="button" value="{phrase var='sms.send__message'}" onclick="Sms.send(); return false;" />
         </div>
         <div class="clear"></div>
     </div>
 </form>
 <div id="sms-success-result">
     <div class="message"></div>
-    <a id="sendMoreSms" href="#" onclick="Sms.sendMoreSms(); return false;">Отправить еще Sms</a>
+    <a id="sendMoreSms" href="#" onclick="Sms.sendMoreSms(); return false;">{phrase var='sms.send_new_message'}</a>
 </div>
