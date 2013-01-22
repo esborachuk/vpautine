0d64aca394e9dd8589177c963feadddd8401b606
home-work <azsxdcfv123>
2013-01-23 00:03:20 +0200

    update user menu

--- module/pautina/template/default/block/profile/usermenu.html.php
+++ module/pautina/template/default/block/profile/usermenu.html.php
@@ -2,7 +2,19 @@
     <ul>
         <?php foreach($this->getVar('menu') as $key => $item): ?>
         <li class="<?php echo $item['class'] ?>">
-            <a href="<?php echo $item['url'] ?>"><span><?php echo $item['label'] ?></span></a>
+            <a href="<?php echo $item['url'] ?>" class="first"></a>
+            <?php if (isset($item['submenu'])): ?>
+                <span class="user-menu-link-wrapper">
+                    <a href="<?php echo $item['url'] ?>" class="sub">
+                        <span><?php echo $item['label'] ?></span>
+                    </a>
+                    <?php foreach($item['submenu'] as $submenu): ?>
+                        <a href="<?php echo $submenu['url'] ?>" class="sub <?php echo $submenu['class'] ?> <?php if (isset($submenu['size'])) echo $submenu['size'] ?>">
+                            <span><?php echo $submenu['label'] ?></span>
+                        </a>
+                    <?php endforeach; ?>
+                </span>
+            <?php endif; ?>
         </li>
         <?php endforeach; ?>
     </ul>
